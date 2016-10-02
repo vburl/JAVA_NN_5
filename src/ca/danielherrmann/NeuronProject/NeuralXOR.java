27ad669998c9fa22419e6f0379868eed0372ec4a
swordfish <vburl@iscte-iul.pt>
2016-10-02 13:15:43 +0100

    first commit

--- src/ca/danielherrmann/NeuronProject/NeuralXOR.java
+++ src/ca/danielherrmann/NeuronProject/NeuralXOR.java
@@ -1,35 +1,39 @@
 package ca.danielherrmann.NeuronProject;
 
 /**
- * A main method, used for testing the functionality of the rest of my code.
- * I specify that my NeuralNet have 2 input Neurons and 1 output Neurons, as well as 2 hidden layer of 2 neurons each. 
- * Creates a NeuralNet with those specifications, and trains it to approximate the XOR function.
- * Training set input values are the integers 0-3, with integers corresponding to inputs based on their binary value.
- * For example, inputting 1 into the net inputs the values 0 and 1 (01), and inputting 3 inputs the values 1 and 1 (11).
- * Once trained, it then prints out its input and its output for all cases of two inputs.
- * Training time differs dramatically each time the example is run depending on the initial random weights. 
+ * A main method, used for testing the functionality of the rest of my code. I
+ * specify that my NeuralNet have 2 input Neurons and 1 output Neurons, as well
+ * as 2 hidden layer of 2 neurons each. Creates a NeuralNet with those
+ * specifications, and trains it to approximate the XOR function. Training set
+ * input values are the integers 0-3, with integers corresponding to inputs
+ * based on their binary value. For example, inputting 1 into the net inputs the
+ * values 0 and 1 (01), and inputting 3 inputs the values 1 and 1 (11). Once
+ * trained, it then prints out its input and its output for all cases of two
+ * inputs. Training time differs dramatically each time the example is run
+ * depending on the initial random weights.
+ * 
  * @author DHermz
  */
 
 public class NeuralXOR {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	NeuralNet firsttest = new NeuralNet(2, 2, 2, 1);
-	TrainingSet firstset = new TrainingSet();
-	
-	firstset.addPair(0, 0);
-	firstset.addPair(1, 1);
-	firstset.addPair(2, 1);
-	firstset.addPair(3, 0);
-	
-	Trainer testtrainer = new Trainer(firsttest, firstset, 0.1, 0.1);
-	testtrainer.trainAll();
+		NeuralNet firsttest = new NeuralNet(2, 2, 2, 1);
+		TrainingSet firstset = new TrainingSet();
 
-	for (int k = 0; k < 4; ++k) {
-	    firsttest.fireAndPrint(k);
+		firstset.addPair(0, 0);
+		firstset.addPair(1, 1);
+		firstset.addPair(2, 1);
+		firstset.addPair(3, 0);
+
+		Trainer testtrainer = new Trainer(firsttest, firstset, 0.1, 0.1);
+		testtrainer.trainAll();
+
+		for (int k = 0; k < 4; ++k) {
+			firsttest.fireAndPrint(k);
+		}
+
 	}
-	
-    }
 
 }
--- src/ca/danielherrmann/NeuronProject/TrainingPair.java
+++ src/ca/danielherrmann/NeuronProject/TrainingPair.java
@@ -1,40 +1,41 @@
 package ca.danielherrmann.NeuronProject;
 
 /**
- * An object of the class TraingingPair defines an input and an output pair, used to create a TrainingSet to train the neural net. 
- * Stores two int values, doutput (the desired output), and an input.
+ * An object of the class TraingingPair defines an input and an output pair,
+ * used to create a TrainingSet to train the neural net. Stores two int values,
+ * doutput (the desired output), and an input.
+ * 
  * @author DHermz
  */
 
 public class TrainingPair {
-    int doutput, input;
+	int doutput, input;
 
-    /*
-     * Constructor: TrainingPair(int in, int out)
-     * Creates an object of the class TrainingPair, setting doutput equal to the specified int, and input equal to the specified int.
-     */
+	/*
+	 * Constructor: TrainingPair(int in, int out) Creates an object of the class
+	 * TrainingPair, setting doutput equal to the specified int, and input equal
+	 * to the specified int.
+	 */
 
-    TrainingPair(int in, int out) {
-	doutput	= out;
-	input = in;
-    }
+	TrainingPair(int in, int out) {
+		doutput = out;
+		input = in;
+	}
 
-    /*
-     * Method: getInput()
-     * Returns the double input.
-     */
+	/*
+	 * Method: getInput() Returns the double input.
+	 */
 
-    public int getInput() {
-	return input;
-    }
-    
-    /*
-     * Method: getDoutput()
-     * Returns the double doutput.
-     */
+	public int getInput() {
+		return input;
+	}
 
-    public int getDoutput() {
-	return doutput;
-    }
+	/*
+	 * Method: getDoutput() Returns the double doutput.
+	 */
+
+	public int getDoutput() {
+		return doutput;
+	}
 
 }
